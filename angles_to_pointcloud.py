80b9dfc55ea84fa1c0d1bd6c229b2b9478b69a76
jl6017 <jl6017@columbia.edu>
2022-10-10 16:52:41 -0400

    occ

--- angles_to_pointcloud.py
+++ angles_to_pointcloud.py
@@ -5,6 +5,8 @@ import os
 import open3d as o3d
 from urdfpy import URDF
 import copy
+import random
+import time
 
 num_links = 3
 robot = URDF.load('arm3dof/urdf/arm3dof.urdf')
@@ -38,10 +40,11 @@ def test_orig_matrix():
 
 
 def read_matrix_and_visualize(path):
-    mesh_base_c = copy.deepcopy(mesh_base)
+    mesh_base_c = copy.deepcopy(mesh_base)  # .compute_vertex_normals()
     mesh_l1_c = copy.deepcopy(mesh_l1)
     mesh_l2_c = copy.deepcopy(mesh_l2)
     mesh_l3_c = copy.deepcopy(mesh_l3)
+
     T = []
     for i in range(1, num_links + 1):
         T.append(np.loadtxt(path + "link%d.csv" % i))
@@ -55,12 +58,59 @@ def read_matrix_and_visualize(path):
     pc_l2 = mesh_l2_c.sample_points_poisson_disk(1000)
     pc_l3 = mesh_l3_c.sample_points_poisson_disk(1000)
 
-    # o3d.visualization.draw_geometries([mesh_base_c, mesh_l1_c, mesh_l2_c, mesh_l3_c])
-    o3d.visualization.draw_geometries([pc_base, pc_l1, pc_l2, pc_l3])
+    o3d.visualization.draw_geometries([mesh_base_c, mesh_l1_c, mesh_l2_c, mesh_l3_c])
+    # o3d.visualization.draw_geometries([pc_base, pc_l1, pc_l2, pc_l3])
+
+
+def prepare_check():
+    mesh_base_c = copy.deepcopy(mesh_base)  # .compute_vertex_normals()
+    mesh_l1_c = copy.deepcopy(mesh_l1)
+    mesh_l2_c = copy.deepcopy(mesh_l2)
+    mesh_l3_c = copy.deepcopy(mesh_l3)
+
+    mesh_base_c = o3d.t.geometry.TriangleMesh.from_legacy(mesh_base_c)
+    mesh_l1_c = o3d.t.geometry.TriangleMesh.from_legacy(mesh_l1_c)
+    mesh_l2_c = o3d.t.geometry.TriangleMesh.from_legacy(mesh_l2_c)
+    mesh_l3_c = o3d.t.geometry.TriangleMesh.from_legacy(mesh_l3_c)
+
+    scene = o3d.t.geometry.RaycastingScene()
+    scene.add_triangles(mesh_base_c)
+    scene.add_triangles(mesh_l1_c)
+    scene.add_triangles(mesh_l2_c)
+    scene.add_triangles(mesh_l3_c)
+
+    return scene
+
+def check_occupancy(point_pos, scene):
+
+    query_point = o3d.core.Tensor([point_pos], dtype=o3d.core.Dtype.Float32)
+    occupancy = scene.compute_occupancy(query_point)
+
+    return occupancy
 
 
 if __name__ == "__main__":
+    """test orig pos"""
     # test_orig_matrix()
-    for i in range(10):
-        data_path = "transform_data/data_%d/" % i
-        read_matrix_and_visualize(path=data_path)
+
+    """check saved data"""
+    # for i in range(3):
+    #     data_path = "transform_data/data_%d/" % i
+    #     read_matrix_and_visualize(path=data_path)
+
+    """check occupancy"""
+    check_sen = prepare_check()
+    box_size = 0.05
+    points = []
+    for i in range(1000000):
+        points.append([(random.random() - 0.5) * box_size, (random.random() - 0.5) * box_size, random.random() * box_size])
+    print('done')
+
+    st = time.time()
+    for i in range(len(points)):
+        occ = check_occupancy(points[i], check_sen)
+        # if occ[0] == 1:
+        #     print("1")
+
+    et = time.time()
+    print("time used: ", et-st)
